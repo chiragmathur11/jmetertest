e5397cabf2e9aa55cb26930162af40355338f4c5
chiragmathur11 <169991279+chiragmathur11@users.noreply.github.com>
2024-05-28 14:56:35 +0530

    Update workflow_trigger.py

--- testreport/workflow_trigger.py
+++ testreport/workflow_trigger.py
@@ -3,12 +3,12 @@ import os
 import requests
 import sys
 
-TOKEN= str(sys.argv[1])
-OWNER= str(sys.argv[2])
-REPO= str(sys.argv[3])
-Workflow_Name= str(sys.argv[4])
-pl_Baseline_Number= str(sys.argv[5])
-pl_Baseline_Revision = str(sys.argv[6])
+TOKEN= str(sys.argv[11])
+OWNER= str(sys.argv[12])
+REPO= str(sys.argv[13])
+Workflow_Name= str(sys.argv[14])
+pl_Baseline_Number= str(sys.argv[15])
+pl_Baseline_Revision = str(sys.argv[16])
 
 
 print( "the toke value is")
