1081b9d68eb358f14459db26ccfb4f898f8c877e
chiragmathur11 <169991279+chiragmathur11@users.noreply.github.com>
2024-05-28 18:02:28 +0530

    Update workflow_trigger.py

--- testreport/workflow_trigger.py
+++ testreport/workflow_trigger.py
@@ -32,8 +32,8 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
       print(responseValue.content)
 
 
-      responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
-      print("The respoinse message is ",responsevalue.content)
+     # responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
+    #  print("The respoinse message is ",responsevalue.content)
 
 
 trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
