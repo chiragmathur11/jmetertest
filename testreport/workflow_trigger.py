0673c35b6634288ece3046bb99fd45970e0f6cd4
chiragmathur11 <169991279+chiragmathur11@users.noreply.github.com>
2024-05-28 15:30:11 +0530

    Update workflow_trigger.py

--- testreport/workflow_trigger.py
+++ testreport/workflow_trigger.py
@@ -3,12 +3,12 @@ import os
 import requests
 import sys
 
-TOKEN= str(sys.argv[0])
-OWNER= str(sys.argv[0])
-REPO= str(sys.argv[0])
-Workflow_Name= str(sys.argv[0])
-pl_Baseline_Number= str(sys.argv[0])
-pl_Baseline_Revision = str(sys.argv[0])
+TOKEN= str(sys.argv[1])
+OWNER= str(sys.argv[2])
+REPO= str(sys.argv[3])
+Workflow_Name= str(sys.argv[4])
+pl_Baseline_Number= str(sys.argv[5])
+pl_Baseline_Revision = str(sys.argv[6])
 
 
 print( "the toke value is")
@@ -26,8 +26,14 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
           'revision_number': pl_Baseline_Revision
         }
       }
+
+
       responseValue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print(responseValue.content)
+
+
       responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print("The respoinse message is ",responsevalue.content)
+
+
 trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
